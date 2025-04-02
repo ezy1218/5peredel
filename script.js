0d192560475a5ae07efea5c4214c935109227aba
ezy1218 <ziyoda110@icloud.com>
2025-04-02 23:52:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = prompt("ismingizi kiriting")
+
+
+let idi = prompt("Space idigizni kiriting")
+
+
+let password = prompt("Space passwordingizni kiriting")
+const userData = {name, idi, password}
+console.log(userData)
